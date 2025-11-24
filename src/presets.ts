aff4461041398e552b1b7b04f8ef5be7f3b715d6
bryce <50299051+bryce-seifert@users.noreply.github.com>
2025-11-24 11:56:34 -0800

    add separate presets for select + take

--- src/presets.ts
+++ src/presets.ts
@@ -6,7 +6,7 @@ export function UpdatePresets(self: UtahScientificInstance): void {
 		const sourceName = i.label
 		const sourceId = i.id
 		presets[`source_${sourceId}`] = {
-			category: 'Sources (by name)',
+			category: 'Sources (Select Only)',
 			name: `Source ${sourceName}`,
 			type: 'button',
 			style: {
@@ -22,6 +22,54 @@ export function UpdatePresets(self: UtahScientificInstance): void {
 							actionId: 'select_source_name',
 							options: {
 								source: sourceId,
+								take: false,
+							},
+						},
+					],
+					up: [],
+				},
+			],
+			feedbacks: [
+				{
+					feedbackId: 'selected_source',
+					options: {
+						source: sourceId,
+					},
+					style: {
+						color: combineRgb(0, 0, 0),
+						bgcolor: combineRgb(102, 255, 255),
+					},
+				},
+				{
+					feedbackId: 'source_dest_route',
+					options: {
+						source: sourceId,
+					},
+					style: {
+						color: combineRgb(0, 0, 0),
+						bgcolor: combineRgb(255, 191, 128),
+					},
+				},
+			],
+		}
+		presets[`source_${sourceId}_take`] = {
+			category: 'Sources (Select & Take)',
+			name: `Source ${sourceName} (Take)`,
+			type: 'button',
+			style: {
+				text: '$(bpspanel:source_' + sourceId + '_name)',
+				size: '14',
+				color: combineRgb(255, 255, 255),
+				bgcolor: combineRgb(0, 0, 0),
+			},
+			steps: [
+				{
+					down: [
+						{
+							actionId: 'select_source_name',
+							options: {
+								source: sourceId,
+								take: true,
 							},
 						},
 					],
@@ -52,11 +100,34 @@ export function UpdatePresets(self: UtahScientificInstance): void {
 			],
 		}
 	}
+	presets[`source_select_take`] = {
+		category: 'Sources (Select Only)',
+		name: `Take Button`,
+		type: 'button',
+		style: {
+			text: 'Take',
+			size: '14',
+			color: combineRgb(255, 255, 255),
+			bgcolor: combineRgb(0, 0, 0),
+		},
+		steps: [
+			{
+				down: [
+					{
+						actionId: 'take',
+						options: {},
+					},
+				],
+				up: [],
+			},
+		],
+		feedbacks: [],
+	}
 	for (const i of self.router.state.destinationNames) {
 		const destinationName = i.label
 		const destinationId = i.id
 		presets[`destination_${destinationId}`] = {
-			category: 'Destinations (by name)',
+			category: 'Destinations',
 			name: `Destination ${destinationName}`,
 			type: 'button',
 			style: {
