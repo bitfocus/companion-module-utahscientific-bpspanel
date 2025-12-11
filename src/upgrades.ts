9bb60c18a2078e1a5f68565a7334179c7711def5
bryce <50299051+bryce-seifert@users.noreply.github.com>
2025-12-11 14:54:22 -0800

    make behavior consistent for v2 -> v3 source buttons

--- src/upgrades.ts
+++ src/upgrades.ts
@@ -1,4 +1,28 @@
-import type { CompanionStaticUpgradeScript } from '@companion-module/base'
+import type {
+	CompanionUpgradeContext,
+	CompanionStaticUpgradeProps,
+	CompanionStaticUpgradeResult,
+	CompanionStaticUpgradeScript,
+} from '@companion-module/base'
 import type { ModuleConfig } from './config.js'
 
-export const UpgradeScripts: CompanionStaticUpgradeScript<ModuleConfig>[] = []
+export const UpgradeScripts: CompanionStaticUpgradeScript<ModuleConfig>[] = [
+	function v3_0_0(_context: CompanionUpgradeContext<ModuleConfig>, props: CompanionStaticUpgradeProps<ModuleConfig>) {
+		const changes: CompanionStaticUpgradeResult<ModuleConfig> = {
+			updatedConfig: null,
+			updatedActions: [],
+			updatedFeedbacks: [],
+		}
+
+		for (const action of props.actions) {
+			if (action.actionId === 'select_source_name') {
+				if (action.options.take === undefined) {
+					action.options.take = false
+					changes.updatedActions.push(action)
+				}
+			}
+		}
+
+		return changes
+	},
+]
